c6915edaf7099a3008d871092737f4c13687c415
Wendell230 <wendell230.olegario@gmail.com>
2024-09-02 22:32:40 -0300

    iniciando o projeto com o calendario

---
+++ src/page/components/eventosPadrao.jsx
@@ -0,0 +1,22 @@
+const eventosPadrao = [
+    {
+        id:1,
+        title: 'Atividade 1',
+        start: new Date(2024,8,2,10,0),
+        end: new Date(2024,8,3,5,0),
+        desc: 'Nossa primeira atividade',
+        color: 'red',
+        tipo: 'atividade',
+    },
+    {
+        id:2,
+        title: 'Atividade 2',
+        start: new Date(2024,8,3,10,0),
+        end: new Date(2024,8,4,5,0),
+        desc: 'Nossa segunda atividade',
+        color: 'blue',
+        tipo: 'atividade',
+    }
+]
+
+export default eventosPadrao
